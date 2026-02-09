8722056c01f96967663b7bbd2e54ff181617d87f
User Artem <arzer@tut.by>
2026-02-09 17:09:55 +0300

    Второй раз тесты гитхаба

--- src/ru/yandex/practicum/gym/Timetable.java
+++ src/ru/yandex/practicum/gym/Timetable.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Timetable {
 
-    private Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>>/* как это хранить??? */ timetable;
+    private Map<DayOfWeek, TreeMap<TimeOfDay, List<TrainingSession>>> timetable;
 
     public Timetable() {
         timetable = new HashMap<>();
@@ -12,6 +12,7 @@ public class Timetable {
             timetable.put(day, new TreeMap<>(TIME_COMPARATOR));
         }
     }
+
     public void addNewTrainingSession(TrainingSession trainingSession) {
         //сохраняем занятие в расписании
         DayOfWeek day = trainingSession.getDayOfWeek();
@@ -24,7 +25,7 @@ public class Timetable {
         daySchelude.put(time, sessionsAtTime);
     }
 
-    public /* непонятно, что возвращать */List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
+    public List<TrainingSession> getTrainingSessionsForDay(DayOfWeek dayOfWeek) {
         TreeMap<TimeOfDay, List<TrainingSession>> daySchelude = timetable.get(dayOfWeek);
         List<TrainingSession> allSessions = new ArrayList<>();
 
@@ -32,23 +33,22 @@ public class Timetable {
             allSessions.addAll(daySchelude.get(time));
         }
         return allSessions;
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
     }
 
-    public /* непонятно, что возвращать */List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
+    public List<TrainingSession> getTrainingSessionsForDayAndTime(DayOfWeek dayOfWeek, TimeOfDay timeOfDay) {
         TreeMap<TimeOfDay, List<TrainingSession>> daySchelude = timetable.get(dayOfWeek);
         return daySchelude.getOrDefault(timeOfDay, new ArrayList<>());
-        //как реализовать, тоже непонятно, но сложность должна быть О(1)
     }
-private static final Comparator<TimeOfDay> TIME_COMPARATOR = new Comparator<TimeOfDay>() {
-    @Override
-    public int compare(TimeOfDay t1, TimeOfDay t2) {
-        if (t1.getHours() != t2.getHours()) {
-            return Integer.compare(t1.getHours(), t2.getHours());
+
+    private static final Comparator<TimeOfDay> TIME_COMPARATOR = new Comparator<TimeOfDay>() {
+        @Override
+        public int compare(TimeOfDay t1, TimeOfDay t2) {
+            if (t1.getHours() != t2.getHours()) {
+                return Integer.compare(t1.getHours(), t2.getHours());
+            }
+            return Integer.compare(t1.getMinutes(), t2.getMinutes());
         }
-        return Integer.compare(t1.getMinutes(), t2.getMinutes());
-    }
-};
+    };
 
     private static class CoachTrainingCount {
         Coach coach;
@@ -59,6 +59,7 @@ private static final Comparator<TimeOfDay> TIME_COMPARATOR = new Comparator<Time
             this.count = count;
         }
     }
+
     public List<CoachTrainingCount> getCountByCoaches() {
         Map<String, Integer> coachCountMap = new HashMap<>();
 
@@ -67,7 +68,7 @@ private static final Comparator<TimeOfDay> TIME_COMPARATOR = new Comparator<Time
             for (List<TrainingSession> sessions : daySchelude.values()) {
                 for (TrainingSession session : sessions) {
                     Coach coach = session.getCoach();
-                    String coachKey =  coach.getSurname() + " " + coach.getName() + " " + coach.getMiddleName();
+                    String coachKey = coach.getSurname() + " " + coach.getName() + " " + coach.getMiddleName();
                     coachCountMap.put(coachKey, coachCountMap.getOrDefault(coachKey, 0) + 1);
                 }
             }
@@ -75,14 +76,15 @@ private static final Comparator<TimeOfDay> TIME_COMPARATOR = new Comparator<Time
         List<CoachTrainingCount> result = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : coachCountMap.entrySet()) {
             String[] nameParts = entry.getKey().split(" ");
-            Coach coach = new Coach(nameParts[0], nameParts[1], nameParts[2] );
+            Coach coach = new Coach(nameParts[0], nameParts[1], nameParts[2]);
             result.add(new CoachTrainingCount(coach, entry.getValue()));
         }
         result.sort((c1, c2) -> Integer.compare(c2.count, c1.count));
 
         return result;
     }
-public static class CoachTrainingCount {
+
+    public static class CoachTrainingCount {
         private Coach coach;
         private int count;
 
@@ -91,15 +93,17 @@ public static class CoachTrainingCount {
             this.count = count;
         }
 
-    public Coach getCoach() {
-        return coach;
-    }
-    public int getCount() {
+        public Coach getCoach() {
+            return coach;
+        }
+
+        public int getCount() {
             return count;
-    }
-    @Override
-    public String toString() {
+        }
+
+        @Override
+        public String toString() {
             return coach.getSurname() + " " + coach.getName() + " - " + count + " тренировок";
+        }
     }
 }
- }
